3f3dc00ab24d4992fba210d7fd995cb406f63bbb
Alex Robinson <ajrob.dev@gmail.com>
2014-04-19 10:13:01 -0600

    Fixes filtering by 'All'. Removes unnecessary debugging code in the blog controller

--- app/scripts/controllers/blog.js
+++ app/scripts/controllers/blog.js
@@ -1,16 +1,19 @@
 'use strict';
 
 angular.module('stoogeBloggerzApp')
-  .controller('BlogCtrl', function ($scope, $location, Blog, Authors) {
+  .controller('BlogCtrl', function ($scope, Blog, Authors) {
   	$scope.blogPosts = Blog.getPosts();
   	$scope.authors = Authors.getAuthors();
   	$scope.search = "";
   	$scope.selectedIndex = 0;
   	$scope.searchByAuthor = function(theAuthor){
-  		$scope.search = theAuthor;
+  		if(theAuthor === "All"){
+  			$scope.search = "";
+  		} else {
+  			$scope.search = theAuthor;
+  		}
   	};
-  	$scope.navigationClick = function(clickEvent, $index, theAuthor){
-  		$scope.event = clickEvent;
+  	$scope.navigationClick = function($index, theAuthor){
   		$scope.selectedIndex = $index;
   		$scope.searchByAuthor(theAuthor);
   	};
